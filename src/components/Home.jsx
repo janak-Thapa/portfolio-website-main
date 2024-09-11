98950eb019fc610c8729e65d61224c4227ec58e5
janak-Thapa <ogjanak1@gmail.com>
2024-09-11 20:03:18 +0545

    correct the portfolio spelling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             <p className='text-gray-500 py-4 max-w-md'>I am a self-taught developer, crafting web applications with passion. Continuously learning and growing, I bring innovation to every project, driven by a love for coding and design.</p>
             <div>
               <Link to='portfolio' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
-                Protfolio
+                Portfolio
                 <span className='group-hover:rotate-90 duration-300'>
                   <MdOutlineKeyboardArrowRight size={25} className='ml-1'/>
                 </span>
