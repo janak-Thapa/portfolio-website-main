e5ac8757b0cfaf3e9270931837eb2d8035f9b1d7
janak <ogjanak1@gmail.com>
2025-05-31 13:47:40 +0545

    make some updates

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,19 +1,19 @@
 
 import gamehub from "../assets/portfolio/gamehub.png";
-import gemini from "../assets/portfolio/gemini.png";
+// import gemini from "../assets/portfolio/gemini.png";
 import ecommerce from "../assets/portfolio/ecommerce.png";
-import AIVoiceChatAssistant from "../assets/portfolio/AIVoiceChatAssistant.png";
+// import AIVoiceChatAssistant from "../assets/portfolio/AIVoiceChatAssistant.png";
 import AIContentGenerator from "../assets/portfolio/AIContentGenerator.png"
-import AiImageGenerator from "../assets/portfolio/AiImageGenerator.png"
+import AINatureIdentifier from "../assets/portfolio/AINatureIdentifier.png"
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      title: "AI Image Generator",
-      src: AiImageGenerator,
-      demoUrl: "https://imagify-black.vercel.app/",
-      codeUrl: "https://github.com/janak-Thapa/Imagify"
+      title: "AI Nature Identifier",
+      src: AINatureIdentifier,
+      demoUrl: "https://biolens-drab.vercel.app/",
+      // codeUrl: "https://github.com/janak-Thapa/Imagify"
     },
     {
       id: 2,
@@ -22,13 +22,13 @@ const Portfolio = () => {
       demoUrl: "https://ai-genie-wtcv.vercel.app/",
       codeUrl: "https://github.com/janak-Thapa/AI-Genie"
     },
-    {
-      id: 3,
-      title: "AI Voice Chat Assistant",
-      src: AIVoiceChatAssistant,
-      demoUrl: "https://ai-voice-chat-assistant.vercel.app/",
-      codeUrl: "https://github.com/janak-Thapa/AI-Voice-Assistant"
-    },
+    // {
+    //   id: 3,
+    //   title: "AI Voice Chat Assistant",
+    //   src: AIVoiceChatAssistant,
+    //   demoUrl: "https://ai-voice-chat-assistant.vercel.app/",
+    //   codeUrl: "https://github.com/janak-Thapa/AI-Voice-Assistant"
+    // },
     {
       id: 4,
       title: "Game Hub",
@@ -36,13 +36,13 @@ const Portfolio = () => {
       demoUrl: 'https://game-hub-nine-ruby-17.vercel.app/',
       codeUrl: 'https://github.com/janak-Thapa/GameHub'
     },
-    {
-      id: 5,
-      title: "Gemini Clone",
-      src: gemini,
-      demoUrl: 'https://gemini-clone-gamma.vercel.app/',
-      codeUrl: 'https://github.com/janak-Thapa/Gemini-clone'
-    },
+    // {
+    //   id: 5,
+    //   title: "Gemini Clone",
+    //   src: gemini,
+    //   demoUrl: 'https://gemini-clone-gamma.vercel.app/',
+    //   codeUrl: 'https://github.com/janak-Thapa/Gemini-clone'
+    // },
     {
       id: 6,
       title: "Ecommerce",
@@ -61,13 +61,13 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, title, src, demoUrl, codeUrl }) => (
+          {portfolios.map(({ id, title, src, demoUrl }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg bg-opacity-30 bg-gray-800">
               <img src={src} alt={title} className="rounded-md duration-200 hover:scale-105" />
               <h2 className="text-center text-xl font-bold mt-4">{title}</h2>
               <div className="flex items-center justify-center">
                 <a href={demoUrl} target="_blank" rel="noopener noreferrer" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 bg-blue-500 text-white rounded-md text-center">Demo</a>
-                <a href={codeUrl} target="_blank" rel="noopener noreferrer" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 bg-green-500 text-white rounded-md text-center">Code</a>
+                {/* <a href={codeUrl} target="_blank" rel="noopener noreferrer" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 bg-green-500 text-white rounded-md text-center">Code</a> */}
               </div>
             </div>
           ))}
